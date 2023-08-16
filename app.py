3ee07cf3d68a96fd181b311526ef68d677b72618
TechStudent10 <techstudent10yt@gmail.com>
2023-08-16 17:38:42 -0600

    prod changes

--- app.py
+++ app.py
@@ -1,69 +1,16 @@
 from flask import *
 from dotenv import load_dotenv
-import os, json
-from io import BytesIO
-from PIL import Image, ImageDraw
+import os
 
-load_dotenv()
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    
+    from mainBlueprint import app as mainBlueprint
+    app.register_blueprint(mainBlueprint)
 
-names = {}
-with open("names.json", "r", encoding="utf8") as file:
-    names: dict = json.load(file)
+    return app
 
-creditsList = []
-with open("credits.txt", "r", encoding="utf8") as file:
-    creditsList = file.read().split("\n")
-
-def serve_pil_image(pil_img: Image):
-    img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
-
-# yeah I have no clue what to name this
-def exitIfNotInFiles(name: str):
-    if name.lower() not in names.keys():
-        if name.upper() not in creditsList:
-            return render_template("cannotFindName.html", name=name), 404
-        return render_template("nameInCredits.html", name=name)
-    return None
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/name/")
-def nameIndex():
-    return redirect(url_for("index"))
-
-@app.route("/name/<name>")
-def showName(name: str):
-    result = exitIfNotInFiles(name)
-    if result: return result
-
-    return render_template("name.html", name=name)
-
-@app.route("/getCroppedImage/<name>")
-def getCroppedImage(name: str):
-    result = exitIfNotInFiles(name)
-    if result: return result
-
-    dimensions = names[name.lower()]
-    top = dimensions["top"]
-    left = dimensions["left"]
-    right = left + dimensions["width"]
-    bottom = top + dimensions["height"]
-
-    rectPadding = 5
-
-    image = Image.open("gd10.jpg").convert("RGB")
-    draw = ImageDraw.Draw(image)
-    draw.rectangle([left - rectPadding, top - rectPadding, left + dimensions["width"] + rectPadding, top + dimensions["height"] + rectPadding], width=2, outline="red")
-
-    imgPadding = 150
-    cropped_image = image.crop((left - imgPadding, top - imgPadding, right + imgPadding, bottom + imgPadding))
-
-    return serve_pil_image(cropped_image)
-
-app.run(debug=os.getenv("DEBUG") == "TRUE", port=os.getenv("PORT"), host="0.0.0.0")
+if __name__ == "__main__":
+    load_dotenv()
+    app = create_app()
+    app.run(debug=os.getenv("DEBUG") == "TRUE", port=os.getenv("PORT"), host="0.0.0.0")
---
+++ mainBlueprint.py
@@ -0,0 +1,65 @@
+from flask import *
+import json
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+app = Blueprint("main", __name__, url_prefix="/")
+
+names = {}
+with open("names.json", "r", encoding="utf8") as file:
+    names: dict = json.load(file)
+
+creditsList = []
+with open("credits.txt", "r", encoding="utf8") as file:
+    creditsList = file.read().split("\n")
+
+def serve_pil_image(pil_img: Image):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+# yeah I have no clue what to name this
+def exitIfNotInFiles(name: str):
+    if name.lower() not in names.keys():
+        if name.upper() not in creditsList:
+            return render_template("cannotFindName.html", name=name), 404
+        return render_template("nameInCredits.html", name=name)
+    return None
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/name/")
+def nameIndex():
+    return redirect(url_for("index"))
+
+@app.route("/name/<name>")
+def showName(name: str):
+    result = exitIfNotInFiles(name)
+    if result: return result
+
+    return render_template("name.html", name=name)
+
+@app.route("/getCroppedImage/<name>")
+def getCroppedImage(name: str):
+    result = exitIfNotInFiles(name)
+    if result: return result
+
+    dimensions = names[name.lower()]
+    top = dimensions["top"]
+    left = dimensions["left"]
+    right = left + dimensions["width"]
+    bottom = top + dimensions["height"]
+
+    rectPadding = 5
+
+    image = Image.open("gd10.jpg").convert("RGB")
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([left - rectPadding, top - rectPadding, left + dimensions["width"] + rectPadding, top + dimensions["height"] + rectPadding], width=2, outline="red")
+
+    imgPadding = 150
+    cropped_image = image.crop((left - imgPadding, top - imgPadding, right + imgPadding, bottom + imgPadding))
+
+    return serve_pil_image(cropped_image)
